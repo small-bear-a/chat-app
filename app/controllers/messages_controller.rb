5bdd6d72c3a4a1eb608d50bb7c847900f35133e0
small-bear-a <77313049+small-bear-a@users.noreply.github.com>
2021-01-21 18:16:54 +0900

    画像表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,10 +23,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
-  end
-
-  def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
